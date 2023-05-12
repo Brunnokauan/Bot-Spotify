1be0acbaadd2fbde4c8d7a20a3d61b2662158b99
Brunnokauan <brunno_kauan@hotmail.com>
2023-05-12 06:47:15 -0300

    Add primeiro comando do bot

---
+++ access.py
@@ -0,0 +1,34 @@
+import spotipy.util as util
+from spotipy.oauth2 import SpotifyOAuth
+from dotenv import load_dotenv
+import os
+# import spotipy
+# from requests import post, get
+import requests
+# import json
+
+username = 'bot-spotify'
+redirect_uri = 'http://localhost:8888/callback'
+scope = 'user-read-recently-played user-read-private user-read-email'
+
+load_dotenv()
+
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
+
+# token = spotipy.Spotify(auth_manager=SpotifyOAuth(username=username, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, scope=scope))
+token = util.prompt_for_user_token(username=username, client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, scope=scope)
+
+def web_api(endpoint, method, body):
+    data = {"grant_type": "client_credentials"}
+    if (method == 'get'):
+        res = requests.get(url=f"https://api.spotify.com/{endpoint}",
+              headers={"Authorization": f"Bearer {token}"}, data=data)
+    elif (method == 'post'):
+        res = requests.post(url=f"https://api.spotify.com/{endpoint}",
+              headers={"Authorization": f'Bearer {token}', "Content-Type": "application/x-www-form-urlencoded"},
+              json=body)
+    return res.text
+
+# result = web_api('v1/me','get')
+# print(result)
