938c78cb4c89601470108a4259c323ef08dee9f2
Brunnokauan <brunno_kauan@hotmail.com>
2023-05-14 21:28:05 -0300

    Correção de segurança

--- bot_discord.py
+++ bot_discord.py
@@ -3,14 +3,18 @@ from discord import app_commands
 from main import top_five
 import os
 from time import sleep
+from dotenv import load_dotenv
+
+load_dotenv()
 
 token_bot = os.getenv('TOKEN_BOT')
+id_servidor = os.getenv('SERVIDOR_ID')
 
 saudacoes = ['oi', 'Oi', 'olá', 'Olá']
 
 class MyClient(discord.Client):
     async def on_ready(self):
-        await tree.sync(guild=discord.Object(id=1077791239732199575))
+        await tree.sync(guild=discord.Object(id=id_servidor))
         print(f'Logged on as {self.user}')
 
 
@@ -30,12 +34,12 @@ client = MyClient(intents=intents)
 
 tree = app_commands.CommandTree(client)
 # Digita comando
-@tree.command(guild=discord.Object(id=1077791239732199575), name='topSongs', description='Top músicas mais ouvidas deste mês')
+@tree.command(guild=discord.Object(id=id_servidor), name='top-songs', description='Top músicas mais ouvidas deste mês')
 async def topSongs(interaction: discord.Interaction, num:int=5):
     await interaction.response.send_message(top_five(num), ephemeral=True)
 
 # Seleciona comando. OBS: clique botão direito minha mensagem, em apps
-# @tree.context_menu(name="Teste", guild=discord.Object(id=1077791239732199575))
+# @tree.context_menu(name="Teste", guild=discord.Object(id=id_servidor))
 # async def teste(interaction: discord.Interaction, message:discord.Message):
 #     await interaction.response.send_message(f"Estou funcionando!")
 
